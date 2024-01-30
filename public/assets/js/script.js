350767af8848f0d18fdf59e0f6742a4a13601581
Ega Alifya Firnando <egaalifyaf@gmail.com>
2024-01-30 14:21:11 +0700

    initial commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,31 @@
+const days1 = document.querySelector('#days');
+const hours1 = document.querySelector('#hours');
+const minutes1 = document.querySelector('#minutes');
+const seconds1 = document.querySelector('#seconds');
+const newYears = 'Feb 14 2024 00:00:00';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+
+    days1.innerHTML = formatTime(days);
+    hours1.innerHTML = formatTime(hours);
+    minutes1.innerHTML = formatTime(minutes);
+    seconds1.innerHTML = formatTime(seconds);
+    // console.log(seconds)
+    // console.log(minutes)
+    // console.log(hours)
+}
+countdown();
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown, 1000);
